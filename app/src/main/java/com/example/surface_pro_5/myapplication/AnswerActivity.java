45f46a22962f5588a5e6a926dbf1af7ca4e126c6
qwer14788lll <qwer14788lll@163.com>
2019-02-09 15:21:52 +0800

    最低版本为 Android 8.0

--- app/src/main/java/com/example/surface_pro_5/myapplication/AnswerActivity.java
+++ app/src/main/java/com/example/surface_pro_5/myapplication/AnswerActivity.java
@@ -2,6 +2,7 @@ package com.example.surface_pro_5.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -31,5 +32,9 @@ public class AnswerActivity extends AppCompatActivity {
         data.putExtra(EXTRA_ANSWER_SHOWN, "您已经查看了答案");
         //data.putExtra("answer_shown", "您已经查看了答案");
         setResult(RESULT_OK, data);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//兼容性判断
+            //Build.VERSION.SDK_INT常量代表了Android设备的版本号
+        }
     }
 }
