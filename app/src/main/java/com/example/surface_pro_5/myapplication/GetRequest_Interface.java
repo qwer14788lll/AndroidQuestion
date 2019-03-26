e24f680d62da363caccc7293fcdd0c0ff5a85f0f
qwer14788lll <qwer14788lll@163.com>
2019-03-26 19:10:00 +0800

    2019-3-26

--- app/src/main/java/com/example/surface_pro_5/myapplication/GetRequest_Interface.java
+++ app/src/main/java/com/example/surface_pro_5/myapplication/GetRequest_Interface.java
@@ -9,8 +9,8 @@ public interface GetRequest_Interface {//用于描述网络请求的接口
     //采用 注解 描述 网络请求参数
     //@GET("ajax.php?a=fy&f=auto&t=auto&w=hello%20world")//从指定的Url请求数据
     @POST("ajax.php?a=fy&f=auto&t=auto&w=")
-    //Call<Translation> getCall();
-    Call<Translation> getCall(@Field("i") String targetSentence);
+    Call<Translation> getCall();
+    //Call<Translation> getCall(@Field("i") String targetSentence);
     // 注解里传入 网络请求 的部分URL地址
     // 网络适配器 把网络请求的URL分成了两部分：一部分放在 网络适配器 对象里，另一部分放在网络请求接口里
     // 如果接口里的url是一个完整的网址，那么放在 网络适配器 对象里的URL可以忽略
